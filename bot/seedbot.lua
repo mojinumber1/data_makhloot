e3fba5592e9bbc5720f16b64c1ee196d1ac568c3
mojinumber1 <mojinumber1@yahoo.com>
2016-11-18 19:00:14 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,8 +225,7 @@ function create_config( )
     "all",
     "leave_ban",
     "admin"
-    },sudo_users = {our id}, 
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    },sudo_users = { 57675417 },--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
